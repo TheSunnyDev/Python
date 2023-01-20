e79e0370e8a08896d57d671c63312c7b56d71448
thesunnydev <109054640+TheSunnyDev@users.noreply.github.com>
2023-01-19 22:33:35 -0800

    add new solution

---
+++ apple-turnover.py
@@ -0,0 +1,9 @@
+# Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+Solution:
+def apple(x):
+    x = int(x)
+    if x ** 2 > 1000:
+        return "It's hotter than the sun!!"
+    else:
+        return "Help yourself to a honeycomb Yorkie for the glovebox."
